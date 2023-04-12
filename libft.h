3b3b200aeb8dc525b23fce5b3b0515f6e0e902b7
Jordan Luiz Dourado Filgueiras <jfilguei@c1r13s5.42porto.com>
2023-04-12 22:20:00 +0100

    First commit

---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfilguei <jfilguei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/12 18:42:49 by jfilguei          #+#    #+#             */
+/*   Updated: 2023/04/12 19:26:04 by jfilguei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <limits.h>
+# include <unistd.h>
+# include <ctype.h>
+
+
+
+
+
+#endif
